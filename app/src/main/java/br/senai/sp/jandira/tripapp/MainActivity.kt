7e87d4a7286bdac80bb5810cb1e157fb8370638b
FELIPE DE OLIVEIRA FLORENCIO <22282108@dsn127m1247443.cfp127aluno.local>
2023-02-28 11:25:38 -0300

    Sharing to cloud

---
+++ app/src/main/java/br/senai/sp/jandira/tripapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package br.senai.sp.jandira.tripapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.senai.sp.jandira.tripapp.ui.theme.TripAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TripAppTheme {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TripAppTheme {
+        Greeting("Android")
+    }
+}
