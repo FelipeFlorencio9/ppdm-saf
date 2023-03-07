dd33593478ede74b64630cb9f29cf2e648ab6962
FELIPE DE OLIVEIRA FLORENCIO <22282108@dsn127m1247443.cfp127aluno.local>
2023-03-07 08:50:36 -0300

    Created Sign In strings

--- app/src/main/java/br/senai/sp/jandira/tripapp/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/tripapp/MainActivity.kt
@@ -3,12 +3,16 @@ package br.senai.sp.jandira.tripapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.textInputServiceFactory
 import androidx.compose.ui.tooling.preview.Preview
 import br.senai.sp.jandira.tripapp.ui.theme.TripAppTheme
 
@@ -17,21 +21,44 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TripAppTheme {
-
+                TripMainScreen()
             }
         }
     }
 }
-
+@Preview(showSystemUi = true)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun TripMainScreen(){
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TripAppTheme {
-        Greeting("Android")
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Card(
+                backgroundColor = Color.Blue
+            ) {
+                Text(text = "test")
+            }
+            Row() {
+
+            }
+            Button(
+                onClick = { /*TODO*/ },
+
+            ) {
+
+            }
+            Row() {
+
+            }
+            Card() {
+
+            }
+
+
+        }
+
     }
+
+
 }
+
+
