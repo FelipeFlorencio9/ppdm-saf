6a17bc153e60f2523aa2ccbad74f808c5293eaa3
Felipe Florencio <felipeflorencio9@github.com>
2023-03-14 11:25:28 -0300

    Init user model

---
+++ app/src/main/java/br/senai/sp/jandira/tripapp/HomeActivity.kt
@@ -0,0 +1,57 @@
+package br.senai.sp.jandira.tripapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import br.senai.sp.jandira.tripapp.ui.theme.TripAppTheme
+
+class HomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TripAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun HomeScreen() {
+    //Body
+    Column(
+        modifier = Modifier.background(Color.Green)
+    ) {
+        //Profile
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Image(painter = painterResource(id = R.drawable.susanna_hoffs), contentDescription = stringResource(
+                id = R.string.user_susanna)
+            )
+        }
+    }
+}
